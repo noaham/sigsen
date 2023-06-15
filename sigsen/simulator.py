df9d72a0cf92f8438791aeea3ffda52916237951
Noah White <noah.alexander.white@gmail.com>
2023-06-15 10:47:27 +1000

    added display and grid functionality

--- sigsen/simulator.py
+++ sigsen/simulator.py
@@ -4,6 +4,11 @@ Module providing utilities for the purpose of simulating data collected by senso
 
 import numpy as np
 from dataclasses import dataclass
+import matplotlib.pyplot as plt
+
+
+def dist(p: np.ndarray, q: np.ndarray) -> float:
+    return np.sqrt(np.sum(np.square(p-q)))
 
 
 @dataclass
@@ -14,6 +19,20 @@ class SensorData:
     sensors: np.ndarray
     sources: np.ndarray
     signals: np.ndarray
+
+    def display(self):
+        fig, ax = plt.subplots()
+        ax.scatter(
+            x=self.sensors[:, 0],
+            y=self.sensors[:, 1],
+            c='blue'
+        )
+        ax.scatter(
+            x=self.sources[:, 0],
+            y=self.sources[:, 1],
+            c='red'
+        )
+        plt.show()
 
 
 class SensorField:
@@ -26,7 +45,7 @@ class SensorField:
             xmax: float,
             ymin: float,
             ymax: float,
-            sensor_locations: list[tuple[float, float]] | np.ndarray = None,
+            sensor_locations: list[tuple[float, float]] | np.ndarray = np.empty((0,2)),
     ) -> None:
         """
         random, grid, specified
@@ -35,8 +54,7 @@ class SensorField:
         self.xmax = xmax
         self.ymin = ymin
         self.ymax = ymax
-        self.sensors = np.array((0, 2))
-        self._name_counter = 0
+        self.sensors = sensor_locations
 
         self.add_sensors(sensor_locations)
 
@@ -67,8 +85,31 @@ class SensorField:
     def add_random_sensors(self, num: int = 1) -> None:
         self.add_sensors(self._random_locations(num))
 
+    def add_sensor_grid(
+            self,
+            shape: tuple[int, int],
+            extent: tuple[float, ...] | None = None,
+    ) -> None:
+        if extent is None:
+            extent = (self.xmin, self.xmax, self.ymin, self.ymax)
+
+        xstep = (extent[1] - extent[0]) / (shape[1] - 1)
+        ystep = (extent[3] - extent[2]) / (shape[0] - 1)
+
+        sensors = np.array([
+            [
+                extent[0] + xstep * i,
+                extent[2] + ystep * j,
+            ] for i in range(shape[1]) for j in range(shape[0])
+        ])
+
+        self.add_sensors(sensors)
+
+
     def display(self) -> None:
-        pass
+        fig, ax = plt.subplots()
+        ax.scatter(self.sensors[:,0], self.sensors[:,1])
+        plt.show()
 
     def gen_data(
             self,
@@ -79,6 +120,11 @@ class SensorField:
     ) -> SensorData:
         if source_locs is None:
             source_locs = self._random_locations(num_sources)
-        signals = np.sqrt(np.matmul(self.sensors, source_locs)) / signal_speed
-        signals += self.rng.normal(0, noise, signals.shape)
+
+        signals = np.array([
+            [
+                dist(source, sensor) / signal_speed for source in source_locs
+            ] for sensor in self.sensors
+        ]) + self.rng.normal(0, noise, (len(self.sensors), num_sources))
+
         return SensorData(self.sensors, source_locs, signals)
