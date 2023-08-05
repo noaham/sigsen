9d8669cef9eb573917e6c90a4ac8f38bb3b2c1ba
Noah White <noah.alexander.white@gmail.com>
2023-08-05 21:47:30 +1000

    Documentation for maxima module

--- sigsen/maxima.py
+++ sigsen/maxima.py
@@ -7,6 +7,20 @@ from itertools import product
 
 
 def max_ij(a: np.ndarray) -> tuple[int, ...]:
+    """
+    Returns the index of the max entry of an array.
+
+    Parameters
+    ----------
+    a : np.ndarray
+        An array
+
+    Returns
+    -------
+    tuple[int, ...]
+        The index of the max entry of `a`.
+
+    """
     return tuple(np.unravel_index(np.argmax(a), a.shape))
 
 
@@ -14,6 +28,22 @@ def loc_in_shape(
         loc: tuple[int, ...],
         shape: tuple[int, ...]
 ) -> bool:
+    """
+    Determines whether a given index is a valid location in an array of specified shape.
+
+    Parameters
+    ----------
+    loc : tuple[int, ...]
+        The index to be tested.
+    shape : tuple[int, ...]
+        The shape of the array.
+
+    Returns
+    -------
+    bool
+        Returns `True` if `loc` is a valid index for an array of shape `shape`'
+
+    """
     if len(loc) != len(shape):
         raise ValueError(f'The index {loc} is not valid for shape {shape}')
     return all(0 <= x < a for x, a in zip(loc, shape))
@@ -23,6 +53,21 @@ def neighbours(
         loc: tuple[int, ...],
         shape: tuple[int, ...]
 ) -> list[tuple[int, ...]]:
+    """
+    Given an entry of an array, return a list of the neighbouring entries.
+
+    Parameters
+    ----------
+    loc : tuple[int, ...]
+        The index of the entry whose neighbours are to be found.
+    shape : tuple[int, ...]
+        The shape of the array.
+
+    Returns
+    -------
+    list[tuple[int, ...]]
+        A list of indices of the neighbouring entries.
+    """
     dim = len(shape)
     if len(loc) != dim or not loc_in_shape(loc, shape):
         raise ValueError(f'The index {loc} is not valid for shape {shape}')
@@ -32,6 +77,22 @@ def neighbours(
 
 
 def lengthen(l: list, n: int) -> list:
+    """
+    Lengthen a list to specified length by repeating its entries.
+
+    Parameters
+    ----------
+    l : list
+        The list to be lengthened.
+    n : int
+        The total length desired.
+
+    Returns
+    -------
+    list
+        The lengthened list.
+
+    """
     idx = 0
     long_l = []
     while len(long_l) < n:
@@ -41,6 +102,22 @@ def lengthen(l: list, n: int) -> list:
 
 
 def maxima(a: np.ndarray, n: int) -> list[tuple[int, ...], ...]:
+    """
+    Returns a list of the local maxima of an array.
+
+    Parameters
+    ----------
+    a : np.ndarray
+        The array whose local maxima are to be found.
+    n : int
+        The number of local maxima desired.
+
+    Returns
+    -------
+    list[tuple[int, ...], ...]
+        A list of indices to the local maxima.
+
+    """
     x = np.copy(a)
     max_locations = []
     while len(max_locations) < n:
